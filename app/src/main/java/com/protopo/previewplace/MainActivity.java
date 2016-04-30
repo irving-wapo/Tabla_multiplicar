d1e21e489f5282ebe81098f82c23e75aeeb6a944
Irving-wapo <nave_wapo@hotmail.com>
2016-04-29 23:34:01 -0500

    04-29 11:31 Desarrollo de  nuevo activity n_p

--- app/src/main/java/com/protopo/previewplace/MainActivity.java
+++ app/src/main/java/com/protopo/previewplace/MainActivity.java
@@ -1,93 +1,89 @@
 package com.protopo.previewplace;
 import android.support.v4.app.FragmentManager;
-
 import android.support.v4.app.DialogFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
-
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
 import android.content.Intent;
-
 import android.widget.Toast;
-
 import java.io.File;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements dialog_nombre_archivo.DialogListener , NavigationView.OnNavigationItemSelectedListener, msg_borrar.DialogListener,menu_lista.DialogListener
-{
+ {
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    protected void onCreate(Bundle savedInstanceState)
+    {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.activity_main);
+         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle( this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        //-----------------------------------
-
-
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            { drawer.closeDrawer(GravityCompat.START); }
+        else
+            { super.onBackPressed();}
     }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
 
-        // Handle navigation view item clicks here.
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         int id = item.getItemId();
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if (id == R.id.nav_nivelacion) {
-
+        if (id == R.id.nav_nivelacion)
+        {
             String ya_llego[] =filtrar_archivos(".nd").toArray(new String[0]);
             Bundle bundle = new Bundle();
             bundle.putStringArray("lista", ya_llego );
             NivelacionDiferencial fragInfo = new NivelacionDiferencial();
             fragInfo.setArguments(bundle);
-            fragmentManager.beginTransaction()
-           .replace(R.id.content_frame,fragInfo).commit();
+            fragmentManager.beginTransaction().replace(R.id.content_frame,fragInfo).commit();
+        }
 
+        if (id == R.id.nav_nivelacion_perfil)
+        {
+            String ya_llego[] =filtrar_archivos(".nd").toArray(new String[0]);
+            Bundle bundle = new Bundle();
+            bundle.putStringArray("lista", ya_llego );
+            NivelacionDiferencial fragInfo = new NivelacionDiferencial();
+            fragInfo.setArguments(bundle);
+            fragmentManager.beginTransaction().replace(R.id.content_frame,fragInfo).commit();
+        }
 
-
-        } else if (id == R.id.nav_configuracion) {
+        if (id == R.id.nav_configuracion)
+        {
             Intent ajustes = new Intent(MainActivity.this, Ajustes.class );
             startActivity(ajustes);
-
-
-
         }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-
-
     }
-
 
 
     public  ArrayList<String> filtrar_archivos(String extencion)
@@ -119,8 +115,6 @@ public class MainActivity extends AppCompatActivity implements dialog_nombre_arc
     {
         msg_borrar objt = new msg_borrar();
         objt.show(getSupportFragmentManager(),"Borrar");
-
-
     }
 
     private void abrir_niv_dif(Boolean estado, String nombre)
@@ -136,14 +130,13 @@ public class MainActivity extends AppCompatActivity implements dialog_nombre_arc
                 startActivity(diferencial);
             }
         }
-        catch(Exception ex)
-        {
-
-        }
+        catch(Exception ex) { }
     }
-    @Override
-    public void aceptar_btn(DialogFragment dialog, String nombre) {
 
+
+    @Override
+    public void aceptar_btn(DialogFragment dialog, String nombre)
+    {
         boolean bandera = true;
         if(!filtrar_archivos(".nd").contains(nombre.concat(".nd")))
         {
@@ -153,9 +146,9 @@ public class MainActivity extends AppCompatActivity implements dialog_nombre_arc
                 if( !Character.isDigit(letra)  &&  !Character.isLetter(letra) ) {  bandera = false; }
             }
 
-            if(bandera){
+            if(bandera)
+            {
                 abrir_niv_dif(true,nombre.concat(".nd"));
-
             }
             else {   Toast.makeText(getApplicationContext(), R.string.msjError_nombre, Toast.LENGTH_SHORT).show();  }
         }
@@ -163,16 +156,11 @@ public class MainActivity extends AppCompatActivity implements dialog_nombre_arc
         {
             Toast.makeText(getApplicationContext(), R.string.msjError_nombre2, Toast.LENGTH_SHORT).show();
         }
-
     }
 
 
     @Override
-    public void cancelar_btn(DialogFragment dialog)
-    {
-
-
-    }
+    public void cancelar_btn(DialogFragment dialog) { }
 
     @Override
     public void si_btn_msg(DialogFragment dialog)
@@ -181,13 +169,15 @@ public class MainActivity extends AppCompatActivity implements dialog_nombre_arc
     }
 
     @Override
-    public void no_btn_msg(DialogFragment dialog) {
+    public void no_btn_msg(DialogFragment dialog)
+    {
         Toast.makeText(getApplicationContext(), "Le dio que NO", Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
-    public void onClickListaDif(DialogFragment dialog, int arg) {
+    public void onClickListaDif(DialogFragment dialog, int arg)
+    {
         Toast.makeText(getApplicationContext(),""+arg,Toast.LENGTH_SHORT).show();
     }
 }
