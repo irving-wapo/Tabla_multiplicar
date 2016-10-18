380450badfb6237881f2e1a4d5751988683c176b
angelicacivil <engjell445@gmail.com>
2016-10-18 11:34:55 -0500

    primer push

--- app/src/main/java/com/protopo/previewplace/acerca.java
+++ app/src/main/java/com/protopo/previewplace/acerca.java
@@ -2,12 +2,25 @@ package com.protopo.previewplace;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class acerca extends AppCompatActivity {
 
+    ImageView img,  img2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca);
+        img = (ImageView) findViewById(R.id.imageView2);
+        img.setImageResource(R.mipmap.icono);
+
+        img2 = (ImageView) findViewById(R.id.imageView3);
+        img2.setImageResource(R.mipmap.acercaden);
+
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.giro);
+        img.startAnimation(animation);
+        ///comentario
     }
 }
